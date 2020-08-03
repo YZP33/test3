f7bccace986279008fbd866048bc69e517058b80
yzp33 <492401547@qq.com>
2020-08-03 11:15:41 +0800

    helloVuejs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  	<h2>{{message}}</h2>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+	name: 'Home',
+	data() {
+		return {
+			message: 'hello Vuejs'
+		}
+	}
 }
 </script>
